1096f02af076adc68e1a30bc3a2f2e3dcfbf729c
Saru-Jayaraman <sarumathijayaraman@gmail.com>
2024-10-14 18:07:28 +0530

    Initial commit

---
+++ src/main/java/se/lexicon/week42_emailsenderapi_springrest/Week42EmailSenderApiSpringRestApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.week42_emailsenderapi_springrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week42EmailSenderApiSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week42EmailSenderApiSpringRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/week42_emailsenderapi_springrest/Week42EmailSenderApiSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.week42_emailsenderapi_springrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week42EmailSenderApiSpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
